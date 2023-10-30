e0dd7e68c958ae5b4a038372baeea4acb7930cc7
luigizampol <luigizampol@gmail.com>
2023-10-29 22:47:14 -0300

    Update settings.py

--- projeto_invista_me/settings.py
+++ projeto_invista_me/settings.py
@@ -77,12 +77,33 @@ WSGI_APPLICATION = 'projeto_invista_me.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+        'NAME': 'postgres', 
+
+        'USER': 'postgres', 
+
+        'PASSWORD': 'RHLz5Fhwb37LvoSJ',
+
+        'HOST': 'db.icwqdwxathetwuwlkroi.supabase.co', 
+
+        'PORT': '5432'
+
     }
+
 }
+
 
 
 # Password validation
