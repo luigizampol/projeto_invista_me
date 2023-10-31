47f6135e8026baa30faf412cff5f1f5a8e80792b
luigizampol <luigizampol@gmail.com>
2023-10-30 21:36:09 -0300

    Update settings.py
    Preparação para Render

--- projeto_invista_me/settings.py
+++ projeto_invista_me/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@671tt4ey@fhu812m4sq9mgz_da5(8++)msl&^s3-tnye5n=w#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -76,13 +76,6 @@ WSGI_APPLICATION = 'projeto_invista_me.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 
 DATABASES = {
 
